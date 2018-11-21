457b23c8129d908472334a7c96d8d29fe3543191
kewinczi <213432@student.pwr.edu.pl>
2018-11-21 13:46:30 +0100

    fetch weather data from api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import React, { Component} from "react";
 import "./App.css";
 import {hot} from "react-hot-loader";
+import Card from  './components/Card'
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    );
-  }
+class App extends Component {
+    constructor() {
+        super();
+        this.state = { 
+            weather: [],
+            isLoading: true 
+        }
+    }
+    
+    fetchWeatherForCity(city) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=84aea1ab45e8d501835d9128346165db`)
+            .then(response => response.json())
+            .then(response => {
+                const weather = [...this.state.weather, response]
+                this.setState({ weather, isLoading: false })
+            })
+    }
+    componentDidMount() {
+        this.fetchWeatherForCity("Aveiro");
+        this.fetchWeatherForCity("Wroclaw");
+        this.fetchWeatherForCity("Porto");
+        this.fetchWeatherForCity("Rybnik");
+
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row d-flex justify-content-center">
+                    {
+                        this.state.isLoading ? <h1>Loading</h1> : 
+                        this.state.weather.map((city, index) => {
+                            return <Card key={index} weather={city} />
+                        })
+                    }
+                </div>
+            </div>
+        );
+    }
+
 }
 
-export default App;
+export default hot(module)(App);
