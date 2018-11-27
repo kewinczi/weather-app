15c1934447e4a0eb11fc7ce91bb5e0ee47af8693
kewinczi <213432@student.pwr.edu.pl>
2018-11-27 22:11:26 +0100

    adjust fetching weather for city and move it to App component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,74 @@ import {hot} from "react-hot-loader";
 import Navbar from './components/Navbar';
 import MainPage from "./components/MainPage";
 import Settings from "./components/Settings";
+import { listOfCities } from './constants';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
     constructor() {
         super();
         this.state = { 
+            weather: [],
+            isLoading: true,
+            isError: false,
             numberOfCities: 1
         }
         this.setNumberOfCities = this.setNumberOfCities.bind(this);
-    }
+        this.fetchWeatherForAllCities = this.fetchWeatherForAllCities.bind(this);
+        this.changeCity = this.changeCity.bind(this);
+        this.addCity = this.addCity.bind(this);
+        this.replaceCity = this.replaceCity.bind(this);
 
+    }
 
     setNumberOfCities(number) {
         this.setState({numberOfCities: number})
     }
 
+    fetchWeatherForCity(city, func, index) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=d99157026a1814b598c98bc8bc02fa34`)
+            .then(response => response.json())
+            .then(response => {
+                func(response, index);
+                this.setState({ 
+                    isLoading: false,
+                    isError: false
+                })
+            }).catch(()=>{
+                this.setState({isError: true});
+            })
+    }
+
+    replaceCity(response, index) {
+        const weather = [...this.state.weather]
+        weather[index] = response
+        this.setState({weather})
+    }
+
+    addCity(response) {
+        const weather = [...this.state.weather, response];
+        this.setState({weather});
+    }
+
+    fetchWeatherForAllCities() {
+        listOfCities.forEach((item) => {
+            this.fetchWeatherForCity(item, this.addCity);
+        })
+    }
+
+    
+    componentDidMount() {
+        this.fetchWeatherForAllCities();
+    }
+
+    changeCity() {
+        const index = 0
+        const city = "Oslo";
+        this.fetchWeatherForCity(city, this.replaceCity, index);
+    };
+
     render() {
+        const { weather, isLoading, isError, numberOfCities } = this.state;
         return (
             <div>
                 <Router>
@@ -29,20 +80,25 @@ class App extends Component {
                         <Route 
                             exact path="/" 
                             render={()=> (
-                                <MainPage numberOfCities={this.state.numberOfCities} />
+                                <MainPage 
+                                    numberOfCities={numberOfCities} 
+                                    weather={weather}
+                                    isLoading={isLoading}
+                                    isError={isError}
+                                />
                             )}
                         />
                         <Route 
                             path="/settings"
                             render={()=> (
                                 <Settings setNumberOfCities={this.setNumberOfCities}
-                                    numberOfCities={this.state.numberOfCities}  
+                                    numberOfCities={numberOfCities}  
                                 />
                             )}
                         />
                     </div>
                 </Router>
-
+                <button onClick={this.changeCity}>Cities</button>
             </div>
 
         );
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,45 +1,9 @@
 import React, { Component} from "react";
 import Card from  './Card'
-import { listOfCities } from '../constants';
 class MainPage extends Component {
-    constructor() {
-        super();
-        this.state = { 
-            weather: [],
-            isLoading: true,
-            isError: false,
-        }
-        this.fetchWeatherForAllCities = this.fetchWeatherForAllCities.bind(this);
-    }
-
-    fetchWeatherForCity(city) {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=d99157026a1814b598c98bc8bc02fa34`)
-            .then(response => response.json())
-            .then(response => {
-                const weather = [...this.state.weather, response]
-                this.setState({ 
-                    weather, 
-                    isLoading: false,
-                    isError: false
-                })
-            }).catch(()=>{
-                this.setState({isError: true});
-            })
-    }
-
-    fetchWeatherForAllCities() {
-        listOfCities.forEach((item) => {
-            this.fetchWeatherForCity(item);
-        })
-    }
-    
-    componentDidMount() {
-        this.fetchWeatherForAllCities();
-    }
 
     render() {
-        const { weather, isLoading, isError } = this.state;
-        const { numberOfCities } = this.props;
+        const { weather, isLoading, isError, numberOfCities } = this.props;
         return (
             <div>
                 <div className="container">
